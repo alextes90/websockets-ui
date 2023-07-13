20a8b275b52e110cc053bfa33981b0cb766e9d09
Aleksandr <a.teslin90@gmail.com>
2023-07-13 11:03:59 +0300

    feat: add registration handler

---
+++ src/handlers/reg.ts
@@ -0,0 +1,57 @@
+import { randomUUID } from 'crypto';
+import { db } from '../db';
+
+export const regHandler = (parsedData: any) => {
+  const parsedReqData = JSON.parse(parsedData.data);
+  const { name, password } = parsedReqData;
+  if (
+    name.length < 5 ||
+    password.length < 5 ||
+    typeof name !== 'string' ||
+    typeof password !== 'string'
+  ) {
+    return {
+      type: 'reg',
+      data: JSON.stringify({
+        name,
+        index: 0,
+        error: true,
+        errorText: 'Invalid reg data',
+      }),
+    };
+  }
+  const user = db.find(({ login }) => login === name);
+  if (user && user.password !== password) {
+    return {
+      type: 'reg',
+      data: JSON.stringify({
+        name,
+        index: 0,
+        error: true,
+        errorText: 'Invalid reg data',
+      }),
+    };
+  } else if (user) {
+    return {
+      type: 'reg',
+      data: JSON.stringify({
+        name,
+        index: user.id,
+        error: false,
+        errorText: '',
+      }),
+    };
+  } else {
+    const playerId = randomUUID();
+    db.push({ id: playerId, login: name, password });
+    return {
+      type: 'reg',
+      data: JSON.stringify({
+        name,
+        index: playerId,
+        error: false,
+        errorText: '',
+      }),
+    };
+  }
+};
