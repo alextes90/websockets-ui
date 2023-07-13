f3052e661244492efda78d83a10e634733ba3971
Aleksandr <a.teslin90@gmail.com>
2023-07-13 11:04:53 +0300

    feat: add attack-handler

---
+++ src/handlers/atackHandler.ts
@@ -0,0 +1,454 @@
+import { ActiveRoom } from '../utils/messageHandler';
+
+export const atackHandler = (parsedData: any, activeRooms: ActiveRoom[]) => {
+  const { gameId, x, y, indexPlayer } = JSON.parse(parsedData.data);
+  const activeGame = activeRooms.find((el) => el.roomId === gameId)!;
+  const activeGameIndex = activeRooms.findIndex((el) => el.roomId === gameId);
+  console.log(x, y);
+  const { ships, websocet, id } = activeGame.players.find(
+    (el) => el.id !== indexPlayer
+  )!;
+  const currWebsocet = activeGame.players.find((el) => el.id === indexPlayer)
+    ?.websocet!;
+  console.log(ships);
+  const attackResultHandler = (ships: any, x: number, y: number) => {
+    let result: 'miss' | 'killed' | 'shot';
+    result = 'miss';
+    let length = 0;
+    let direction = true;
+    let position = { x: 0, y: 0 };
+    const newShips = ships.map((el: any) => {
+      let live = el.live || el.length;
+      for (let i = 0; i < el.length; i++) {
+        if (!el.direction) {
+          if (x === el.position.x + i && y === el.position.y) {
+            if (el.length === 1 || (el.live && el.live === 1)) {
+              result = 'killed';
+              length = el.length;
+              direction = el.direction;
+              position = el.position;
+            } else {
+              live -= 1;
+              result = 'shot';
+            }
+          }
+        } else {
+          if (y === el.position.y + i && x === el.position.x) {
+            if (el.length === 1 || (el.live && el.live === 1)) {
+              result = 'killed';
+              length = el.length;
+              direction = el.direction;
+              position = el.position;
+            } else {
+              live -= 1;
+              result = 'shot';
+            }
+          }
+        }
+      }
+      return {
+        ...el,
+        live,
+      };
+    });
+
+    const curentPlayerIndex = activeRooms[activeGameIndex].players.findIndex(
+      (el: any) => el.id === id
+    );
+    activeRooms[activeGameIndex].players[curentPlayerIndex].ships = newShips;
+
+    return {
+      result,
+      length,
+      direction,
+      position,
+    };
+  };
+
+  const attackResult = attackResultHandler(ships, x, y);
+
+  const missMessage = (x: number, y: number) =>
+    JSON.stringify({
+      type: 'attack',
+      data: JSON.stringify({
+        position: {
+          x,
+          y,
+        },
+        currentPlayer: indexPlayer,
+        status: 'miss',
+      }),
+      id: 0,
+    });
+
+  const opponentsTurnMessage = JSON.stringify({
+    type: 'turn',
+    data: JSON.stringify({
+      currentPlayer: id,
+    }),
+    id: 0,
+  });
+
+  const currentPlayerTurnMessage = JSON.stringify({
+    type: 'turn',
+    data: JSON.stringify({
+      currentPlayer: indexPlayer,
+    }),
+    id: 0,
+  });
+
+  const shotMessage = JSON.stringify({
+    type: 'attack',
+    data: JSON.stringify({
+      position: {
+        x,
+        y,
+      },
+      currentPlayer: indexPlayer,
+      status: 'shot',
+    }),
+    id: 0,
+  });
+
+  const killMessage = (x: number, y: number) =>
+    JSON.stringify({
+      type: 'attack',
+      data: JSON.stringify({
+        position: {
+          x,
+          y,
+        },
+        currentPlayer: indexPlayer,
+        status: 'killed',
+      }),
+      id: 0,
+    });
+
+  if (attackResult.result === 'miss') {
+    currWebsocet.send(missMessage(x, y));
+    websocet?.send(missMessage(x, y));
+    currWebsocet.send(opponentsTurnMessage);
+    websocet?.send(opponentsTurnMessage);
+  } else if (attackResult.result === 'shot') {
+    currWebsocet.send(shotMessage);
+    websocet?.send(shotMessage);
+    currWebsocet.send(currentPlayerTurnMessage);
+    websocet?.send(currentPlayerTurnMessage);
+  } else if (attackResult.result === 'killed') {
+    for (let i = 0; i < attackResult.length; i++) {
+      //horesontal
+
+      if (!attackResult.direction) {
+        //missing
+        if (i === 0) {
+          currWebsocet.send(
+            missMessage(attackResult.position.x - 1, attackResult.position.y)
+          );
+
+          currWebsocet.send(
+            missMessage(
+              attackResult.position.x - 1,
+              attackResult.position.y - 1
+            )
+          );
+
+          currWebsocet.send(
+            missMessage(
+              attackResult.position.x - 1,
+              attackResult.position.y + 1
+            )
+          );
+
+          currWebsocet.send(
+            missMessage(attackResult.position.x, attackResult.position.y + 1)
+          );
+
+          currWebsocet.send(
+            missMessage(attackResult.position.x, attackResult.position.y - 1)
+          );
+
+          //second player
+
+          websocet?.send(
+            missMessage(attackResult.position.x - 1, attackResult.position.y)
+          );
+
+          websocet?.send(
+            missMessage(
+              attackResult.position.x - 1,
+              attackResult.position.y - 1
+            )
+          );
+
+          websocet?.send(
+            missMessage(
+              attackResult.position.x - 1,
+              attackResult.position.y + 1
+            )
+          );
+
+          websocet?.send(
+            missMessage(attackResult.position.x, attackResult.position.y + 1)
+          );
+
+          websocet?.send(
+            missMessage(attackResult.position.x, attackResult.position.y - 1)
+          );
+        }
+
+        if (i === attackResult.length - 1) {
+          currWebsocet.send(
+            missMessage(
+              attackResult.position.x + attackResult.length,
+              attackResult.position.y
+            )
+          );
+
+          currWebsocet.send(
+            missMessage(
+              attackResult.position.x + attackResult.length,
+              attackResult.position.y - 1
+            )
+          );
+
+          currWebsocet.send(
+            missMessage(
+              attackResult.position.x + attackResult.length,
+              attackResult.position.y + 1
+            )
+          );
+
+          currWebsocet.send(
+            missMessage(
+              attackResult.position.x + attackResult.length - 1,
+              attackResult.position.y + 1
+            )
+          );
+
+          currWebsocet.send(
+            missMessage(
+              attackResult.position.x + attackResult.length - 1,
+              attackResult.position.y - 1
+            )
+          );
+
+          //secondPlayer
+
+          websocet?.send(
+            missMessage(
+              attackResult.position.x + attackResult.length,
+              attackResult.position.y
+            )
+          );
+
+          websocet?.send(
+            missMessage(
+              attackResult.position.x + attackResult.length,
+              attackResult.position.y - 1
+            )
+          );
+
+          websocet?.send(
+            missMessage(
+              attackResult.position.x + attackResult.length,
+              attackResult.position.y + 1
+            )
+          );
+
+          websocet?.send(
+            missMessage(
+              attackResult.position.x + attackResult.length - 1,
+              attackResult.position.y + 1
+            )
+          );
+
+          websocet?.send(
+            missMessage(
+              attackResult.position.x + attackResult.length - 1,
+              attackResult.position.y - 1
+            )
+          );
+        }
+
+        //simple miss
+
+        currWebsocet.send(
+          missMessage(attackResult.position.x + i, attackResult.position.y - 1)
+        );
+
+        currWebsocet.send(
+          missMessage(attackResult.position.x + i, attackResult.position.y + 1)
+        );
+
+        websocet?.send(
+          missMessage(attackResult.position.x + i, attackResult.position.y - 1)
+        );
+
+        websocet?.send(
+          missMessage(attackResult.position.x + i, attackResult.position.y + 1)
+        );
+
+        //killed
+        currWebsocet.send(
+          killMessage(attackResult.position.x + i, attackResult.position.y)
+        );
+
+        websocet?.send(
+          killMessage(attackResult.position.x + i, attackResult.position.y)
+        );
+      } else {
+        //vertical
+
+        //missing
+        if (i === 0) {
+          currWebsocet.send(
+            missMessage(attackResult.position.x, attackResult.position.y - 1)
+          );
+
+          currWebsocet.send(
+            missMessage(
+              attackResult.position.x - 1,
+              attackResult.position.y - 1
+            )
+          );
+
+          currWebsocet.send(
+            missMessage(
+              attackResult.position.x + 1,
+              attackResult.position.y - 1
+            )
+          );
+
+          currWebsocet.send(
+            missMessage(attackResult.position.x + 1, attackResult.position.y)
+          );
+
+          currWebsocet.send(
+            missMessage(attackResult.position.x - 1, attackResult.position.y)
+          );
+
+          //second player
+
+          websocet?.send(
+            missMessage(attackResult.position.x, attackResult.position.y - 1)
+          );
+
+          websocet?.send(
+            missMessage(
+              attackResult.position.x - 1,
+              attackResult.position.y - 1
+            )
+          );
+
+          websocet?.send(
+            missMessage(
+              attackResult.position.x + 1,
+              attackResult.position.y - 1
+            )
+          );
+
+          websocet?.send(
+            missMessage(attackResult.position.x + 1, attackResult.position.y)
+          );
+
+          websocet?.send(
+            missMessage(attackResult.position.x - 1, attackResult.position.y)
+          );
+        }
+
+        if (i === attackResult.length - 1) {
+          currWebsocet.send(
+            missMessage(
+              attackResult.position.x,
+              attackResult.position.y + attackResult.length
+            )
+          );
+          currWebsocet.send(
+            missMessage(
+              attackResult.position.x - 1,
+              attackResult.position.y + attackResult.length
+            )
+          );
+          currWebsocet.send(
+            missMessage(
+              attackResult.position.x + 1,
+              attackResult.position.y + attackResult.length
+            )
+          );
+          currWebsocet.send(
+            missMessage(
+              attackResult.position.x + 1,
+              attackResult.position.y + attackResult.length - 1
+            )
+          );
+          currWebsocet.send(
+            missMessage(
+              attackResult.position.x - 1,
+              attackResult.position.y + attackResult.length - 1
+            )
+          );
+
+          //secondPlayer
+          websocet?.send(
+            missMessage(
+              attackResult.position.x,
+              attackResult.position.y + attackResult.length
+            )
+          );
+          websocet?.send(
+            missMessage(
+              attackResult.position.x - 1,
+              attackResult.position.y + attackResult.length
+            )
+          );
+          websocet?.send(
+            missMessage(
+              attackResult.position.x + 1,
+              attackResult.position.y + attackResult.length
+            )
+          );
+          websocet?.send(
+            missMessage(
+              attackResult.position.x + 1,
+              attackResult.position.y + attackResult.length - 1
+            )
+          );
+          websocet?.send(
+            missMessage(
+              attackResult.position.x - 1,
+              attackResult.position.y + attackResult.length - 1
+            )
+          );
+        }
+
+        //simple miss
+
+        currWebsocet.send(
+          missMessage(attackResult.position.x - 1, attackResult.position.y + i)
+        );
+        currWebsocet.send(
+          missMessage(attackResult.position.x + 1, attackResult.position.y + i)
+        );
+        websocet?.send(
+          missMessage(attackResult.position.x - 1, attackResult.position.y + i)
+        );
+        websocet?.send(
+          missMessage(attackResult.position.x + 1, attackResult.position.y + i)
+        );
+
+        //killed
+
+        currWebsocet.send(
+          killMessage(attackResult.position.x, attackResult.position.y + i)
+        );
+        websocet?.send(
+          killMessage(attackResult.position.x, attackResult.position.y + i)
+        );
+      }
+    }
+
+    //turns
+    currWebsocet.send(currentPlayerTurnMessage);
+    websocet?.send(currentPlayerTurnMessage);
+  }
+};
